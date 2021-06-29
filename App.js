32e76a5192bc72962bbf41bab2bf5c9dd42c2295
Michael Yurin <mimimichael.yurin@gmail.com>
2021-06-29 14:41:15 +0300

    global update

--- App.js
+++ App.js
@@ -1,206 +1,159 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Platform,
   StyleSheet,
-  Text, View,
+  Text,
+  View,
   TextInput,
   ScrollView,
   ToastAndroid,
-  Clipboard,
   TouchableOpacity,
   NativeModules,
+  useColorScheme,
 } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome";
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Clipboard from '@react-native-community/clipboard';
 
 const alph = {
   ru: 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.split(''),
   en: 'abcdefghijklmnopqrstuvwxyz'.split(''),
 };
 
-type Props = {};
-export default class App extends Component<Props> {
-  // todo multilanguage interface
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputText: '',
-      decodedText: '',
-    };
-    let langRegionLocale = "en_US";
-    if (Platform.OS === "android") {
-      langRegionLocale = NativeModules.I18nManager.localeIdentifier || "";
-    } else if (Platform.OS === "ios") {
-      langRegionLocale = NativeModules.SettingsManager.settings.AppleLocale || "";
-    }
-    let languageLocale = langRegionLocale.substring(0, 2) || 'en';
-    this.state.languageLocale = languageLocale;
-    this.state.language = languageLocale;
-  }
+const App: () => Node = () => {
+  const [inputText, setInputText] = useState('');
+  const [decodedText, setDecodedText] = useState('');
+  const [language, setLanguage] = useState('en');
+  const [themeStyle, setThemeStyle] = useState();
 
-  // componentDidMount() {
-  //   this.setState({
-  //     inputText: 'test !',
-  //   });
-  // }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.inputText !== prevState.inputText) {
-      this.detectLang();
-      this.caesar();
-    }
-    if (this.state.language !== prevState.language) {
-      this.caesar();
-    }
-    return true;
-  }
-
-  detectLang() {
-    const { inputText } = this.state;
-
-    if (typeof inputText === 'string') {
-      const firstLetter = inputText.split('')
-        .find(char => alph.ru.includes(char) || alph.en.includes(char));
+  const detectLang = input => {
+    if (typeof input === 'string') {
+      const firstLetter = input.split('').find(char => alph.ru.includes(char) || alph.en.includes(char));
       if (firstLetter && alph.ru.includes(firstLetter)) {
-        this.setState({ language: 'ru' });
+        setLanguage('ru');
       } else if (firstLetter && alph.en.includes(firstLetter)) {
-        this.setState({ language: 'en' });
+        setLanguage('en');
       }
     }
-  }
+  };
 
-  setLang() {
-    this.setState({
-      language: this.state.language === 'en' ? 'ru' : 'en',
-    });
-  }
-
-  clearOrPaste() {
-    if (this.state.inputText) {
-      this.setState({ inputText: '' });
+  const clearOrPaste = () => {
+    if (inputText) {
+      setInputText('');
     } else {
-      Clipboard.getString().then((data) => {
-        this.setState({ inputText: data });
-      }).catch((err) => {
-        ToastAndroid.showWithGravityAndOffset(
-          'Clipboard is empty',
-          ToastAndroid.LONG,
-          ToastAndroid.TOP,
-          25,
-          50,
-        );
-      });
+      Clipboard.getString()
+        .then(data => {
+          setInputText(data);
+        })
+        .catch(err => {
+          ToastAndroid.showWithGravityAndOffset('Clipboard is empty', ToastAndroid.LONG, ToastAndroid.TOP, 25, 50);
+        });
     }
-  }
+  };
 
-  caesar() {
-    const { inputText, language } = this.state;
-
-    if (typeof inputText === 'string') {
+  const getCaesarCipher = input => {
+    if (typeof input === 'string') {
       let possibleDecodedPhrases = [];
-      let input = inputText.toLowerCase();
-
       // possibleDecodedPhrases.push(input + ' - IS ORIGINAL'); // ! for debug
 
       for (let shift = 1; shift <= alph[language].length; shift++) {
-        const possibleDecodedPhrase = input.split('').map(char => {
-          if (alph[language].includes(char)) {
-            let index = alph[language].indexOf(char) + shift;
-            while (!alph[language][index]) {
-              index -= alph[language].length;
+        const possibleDecodedPhrase = input
+          .toLowerCase()
+          .split('')
+          .map(char => {
+            if (alph[language].includes(char)) {
+              let index = alph[language].indexOf(char) + shift;
+              while (!alph[language][index]) {
+                index -= alph[language].length;
+              }
+              return alph[language][index];
+            } else {
+              return char;
             }
-            return alph[language][index];
-          } else {
-            return char;
-          }
-        }).join('');
+          })
+          .join('');
 
         possibleDecodedPhrases.push(possibleDecodedPhrase);
       }
-
-      this.setState({ decodedText: possibleDecodedPhrases });
+      return possibleDecodedPhrases;
+    } else {
+      return '';
     }
-  }
+  };
 
-  copy(text) {
+  const copy = text => {
     Clipboard.setString(text);
-    ToastAndroid.showWithGravityAndOffset(
-      'Copied to clipboard',
-      ToastAndroid.LONG,
-      ToastAndroid.TOP,
-      25,
-      50,
-    );
-    this.forceUpdate();
-  }
+    ToastAndroid.showWithGravityAndOffset('Copied to clipboard', ToastAndroid.LONG, ToastAndroid.TOP, 25, 50);
+    // fix: forceUpdate();
+  };
 
-  renderClearAndPaste() {
-    const { inputText } = this.state;
-    return (
-      // <TouchableOpacity style={styles.button}>
-      <Icon
-        style={styles.button}
-        name={inputText === '' ? "paste" : "remove"}
-        onPress={this.clearOrPaste.bind(this)}
-      />
-      // </TouchableOpacity>
-    );
-  }
+  useEffect(() => {
+    let langRegionLocale = 'en_US';
+    if (Platform.OS === 'android') {
+      langRegionLocale = NativeModules.I18nManager.localeIdentifier || '';
+    } else if (Platform.OS === 'ios') {
+      langRegionLocale = NativeModules.SettingsManager.settings.AppleLocale || '';
+    }
+    setLanguage(langRegionLocale.substring(0, 2) || 'en');
+  }, []);
 
-  renderLang() {
-    const { language } = this.state;
-    return (
-      <TouchableOpacity
-        style={styles.button}
-        onPress={this.setLang.bind(this)}
-      >
-        <Text style={styles.centered}>{language.toUpperCase()}</Text>
-      </TouchableOpacity>
-    );
-  }
+  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    setThemeStyle({
+      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+      color: isDarkMode ? Colors.lighter : Colors.darker,
+    });
+  }, [isDarkMode]);
 
-  render() {
-    const { decodedText, inputText } = this.state;
+  useEffect(() => {
+    detectLang(inputText);
+    setDecodedText(getCaesarCipher(inputText));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputText]);
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.inctructions}>Enter some text below:</Text>
-        <View style={styles.controls}>
-          <TextInput
-            style={styles.input}
-            multiline={true}
-            onChangeText={text => this.setState({ inputText: text })}
-            value={inputText}
-          />
-          <View style={styles.controlButtonsBlock}>
-            {this.renderClearAndPaste()}
-            {this.renderLang()}
-          </View>
-        </View>
-        <Text style={styles.inctructions}>Possible variants of Caesar cipher (longpress to copy):</Text>
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          {typeof decodedText === 'object' ? decodedText.map((text, index) =>
-            text ? (
-              <TouchableOpacity
-                key={index}
-                style={styles.touchableView}
-                onLongPress={() => this.copy(text)}
-              >
-                <Text style={styles.codeView}>{index + 1}</Text>
-                <Text style={styles.decodedTextView}>
-                  {text}
-                </Text>
-              </TouchableOpacity>
-            ) : null
-          ) : null}
-        </ScrollView>
+  return (
+    <View style={{...styles.container, ...themeStyle}}>
+      <Text style={{...styles.inctructions, ...themeStyle}}>Enter some text below:</Text>
+
+      <View style={{...styles.controls, themeStyle}}>
+        <TextInput style={{...styles.input, ...themeStyle}} multiline={true} onChangeText={setInputText} value={inputText} />
+
+        <TouchableOpacity style={styles.button} onPress={clearOrPaste}>
+          <Text style={styles.buttonText}>{inputText ? 'clear' : 'paste'}</Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
-}
 
+      {inputText ? (
+        <View style={themeStyle}>
+          <Text style={{color: gray}}>languale is: {language.toUpperCase()}</Text>
+        </View>
+      ) : null}
+
+      {inputText ? (
+        <View style={themeStyle}>
+          <Text style={{...styles.inctructions, ...themeStyle}}>Possible variants of Caesar cipher (longpress to copy):</Text>
+        </View>
+      ) : null}
+
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {typeof decodedText === 'object'
+          ? decodedText.map((text, index) =>
+              text ? (
+                <TouchableOpacity key={index} style={{...styles.touchableView, ...themeStyle}} onLongPress={() => copy(text)}>
+                  <Text style={{...themeStyle, ...styles.codeView}}>{index + 1}:</Text>
+                  <Text style={{...styles.decodedTextView, ...themeStyle}}>{text}</Text>
+                </TouchableOpacity>
+              ) : null,
+            )
+          : null}
+      </ScrollView>
+    </View>
+  );
+};
+
+const orange = '#ff9933';
+const gray = '#AAAAAA';
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     height: '100%',
     width: '100%',
@@ -214,29 +167,27 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     flexWrap: 'nowrap',
-    height: 100,
-  },
-  controlButtonsBlock: {
-    width: '10%',
-    flexDirection: 'column',
+    height: 40,
+    borderColor: orange,
+    borderWidth: 1,
   },
   input: {
     height: '100%',
-    borderColor: '#CCCCCC',
-    borderWidth: 1,
-    width: '90%',
-    flex: 13,
+    flex: 1,
+    marginLeft: 8,
   },
   button: {
-    flex: 1,
-    height: 40,
-    width: 40,
-    padding: 10,
-    textAlign: 'center',
+    marginLeft: 4,
+    height: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
     fontSize: 20,
-    backgroundColor: '#DDDDDD',
-    borderColor: '#CCCCCC',
-    borderWidth: 1,
+    color: orange,
+    marginLeft: 8,
+    marginRight: 8,
+    marginBottom: 4, // todo: should be vertical center
   },
   inctructions: {
     fontSize: 20,
@@ -253,7 +204,7 @@ const styles = StyleSheet.create({
   codeView: {
     fontSize: 16,
     textAlign: 'left',
-    color: '#AAAAAA',
+    color: gray,
     marginRight: 10,
   },
   touchableView: {
@@ -263,5 +214,7 @@ const styles = StyleSheet.create({
   scrollContainer: {
     paddingVertical: 10,
     alignItems: 'flex-start',
-  }
+  },
 });
+
+export default App;
